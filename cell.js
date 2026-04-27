85245b82b4ac8a799113e560e0c861859e6857a9
BLITZ <toewaiaungmwd@gmail.com>
2026-04-28 02:11:32 +0630

    Update cell.js

--- cell.js
+++ cell.js
@@ -8,126 +8,89 @@ function Cell(i, j) {
   this.highlight = false;
   this.kill = false;
   this.value = -1;
+  this.king = false; // King ဖြစ်မဖြစ် မှတ်ရန်
 
   this.color = "black";
   this.nativeColor;
 
-  if (this.j % 2 === 0) {
-    if (this.i % 2 === 0) {
-      this.color = "white";
-      this.nativeColor = "white";
-    } else {
-      this.color = "black";
-      this.nativeColor = "black";
-    }
+  // Board အရောင်သတ်မှတ်ခြင်း
+  if ((this.i + this.j) % 2 === 0) {
+    this.color = "white";
+    this.nativeColor = "white";
   } else {
-    if (this.i % 2 === 0) {
-      this.color = "black";
-      this.nativeColor = "black";
-    } else {
-      this.color = "white";
-      this.nativeColor = "white";
-    }
+    this.color = "black";
+    this.nativeColor = "black";
   }
-  
+
   this.show = function() {
-    if(this.highlight){
-      fill(125, 255, 5);
-    }else if(this.kill){
-      fill(255, 45, 45);
-    }else{
+    if (this.highlight) {
+      fill(125, 255, 5); // ရွှေ့လို့ရသည့်နေရာ (အစိမ်း)
+    } else if (this.kill) {
+      fill(255, 45, 45); // စားလို့ရသည့်နေရာ (အနီ)
+    } else {
       if (this.hover) {
-       fill(255, 100);
+        fill(255, 100);
       } else {
-        if (this.color === "white") {
-          fill(255);
-        } else if (this.color === "black") {
-          fill(0);
-        } else if (this.color == "gray") {
-          fill(51);
-        }
+        if (this.color === "white") fill(255);
+        else if (this.color === "black") fill(0);
+        else if (this.color == "gray") fill(51);
       }
     }
     noStroke();
     rect(this.x, this.y, cellW, cellW);
-    if (this.value === 0) {
-      fill(255);
-      stroke(0);
+
+    // ကျားကောင်များ ဆွဲခြင်း
+    if (this.value !== -1) {
+      if (this.value === 0) {
+        fill(255); // အဖြူ
+        stroke(0);
+      } else {
+        fill(0); // အမည်း
+        stroke(255);
+      }
       ellipseMode(CORNER);
-      ellipse(this.x + 3, this.y + 3, cellW * 0.9);
-    } else if (this.value === 1) {
-      fill(0);
-      stroke(255);
-      ellipseMode(CORNER);
-      ellipse(this.x + 3, this.y + 3, cellW * 0.9);
+      ellipse(this.x + 6, this.y + 6, cellW - 12);
+
+      // King ဖြစ်ရင် "K" စာသားပြရန်
+      if (this.king) {
+        fill(255, 215, 0); // ရွှေရောင်
+        noStroke();
+        textAlign(CENTER, CENTER);
+        textSize(24);
+        text("K", this.x + cellW / 2, this.y + cellW / 2);
+      }
     }
   };
 
   this.contains = function(x, y) {
-    return this.x < x && this.x + cellW > x && this.y < y && this.y + cellW > y;
+    return x > this.x && x < this.x + cellW && y > this.y && y < this.y + cellW;
   };
-  
-  this.checkMoves = function(){
-    var topleft = grid[index(this.i-1, this.j-1)]; 
-    var topright = grid[index(this.i+1, this.j-1)];
-    var bottomleft = grid[index(this.i-1, this.j+1)];
-    var bottomright = grid[index(this.i+1, this.j+1)];
-    
-    if(topleft){
-      if(topleft.value === -1){
-        topleft.highlight = true;
-      }else if(topleft.value === this.value){
-        //nothing
-      }else{
-        var checkIndex = index(topleft.i - 1, topleft.j - 1);
-        if(checkIndex > -1){
-          if(grid[checkIndex].value === -1){
-            topleft.kill = true;
-          }
+
+  this.checkMoves = function() {
+    // Player 1 (အမည်း) သို့မဟုတ် King ဖြစ်ရင် အပေါ်တက်ခွင့်ရှိသည်
+    if (this.value === 1 || this.king) {
+      this.checkDirection(-1, -1); // top-left
+      this.checkDirection(1, -1);  // top-right
+    }
+    // Player 0 (အဖြူ) သို့မဟုတ် King ဖြစ်ရင် အောက်ဆင်းခွင့်ရှိသည်
+    if (this.value === 0 || this.king) {
+      this.checkDirection(-1, 1);  // bottom-left
+      this.checkDirection(1, 1);   // bottom-right
+    }
+  };
+
+  this.checkDirection = function(di, dj) {
+    let target = grid[index(this.i + di, this.j + dj)];
+    if (target) {
+      if (target.value === -1) {
+        target.highlight = true;
+      } else if (target.value !== this.value) {
+        // တစ်ဖက်လူရှိရင် ကျော်စားလို့ရမလား စစ်မည်
+        let jump = grid[index(target.i + di, target.j + dj)];
+        if (jump && jump.value === -1) {
+          target.kill = true;
         }
       }
     }
-    if(topright){
-      if(topright.value === -1){
-        topright.highlight = true;
-      }else if(topright.value === this.value){
-        //nothing
-      }else{
-        var checkIndex = index(topright.i + 1, topright.j - 1);
-        if(checkIndex > -1){
-          if(grid[checkIndex].value === -1){
-            topright.kill = true;
-          }
-        }
-      }
-    }
-    if(bottomleft){
-      if(bottomleft.value === -1){
-        bottomleft.highlight = true;
-      }else if(bottomleft.value === this.value){
-        //nothing
-      }else{
-        var checkIndex = index(bottomleft.i - 1, bottomleft.j + 1);
-        if(checkIndex > -1){
-          if(grid[checkIndex].value === -1){
-            bottomleft.kill = true;
-          }
-        }
-      }
-    }
-    if(bottomright){
-      if(bottomright.value === -1){
-         bottomright.highlight = true;
-      }else if(bottomright.value === this.value){
-        //nothing
-      }else{
-        var checkIndex = index(bottomright.i + 1, bottomright.j + 1);
-        if(checkIndex > -1){
-          if(grid[checkIndex].value === -1){
-            bottomright.kill = true;
-          }
-        }
-      }
-    }
-  }
+  };
 }
