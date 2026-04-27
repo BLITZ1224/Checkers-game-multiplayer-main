880d62ca91084bc40a9c8c018f7ad5f2b4006e41
BLITZ <toewaiaungmwd@gmail.com>
2026-04-28 00:20:09 +0630

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ var rows = (cols = 10);
 var cellW = 60;
 var grid = [];
 var selected, selectedValue;
-var whites = blacks = 10;
+var whites = 10, blacks = 10;
 var turn = 0; 
 var turnValue = -1; // -1 ဆိုရင် ဘယ်သူမှ မဟုတ်သေးဘူး
 var gameover = false;
@@ -26,15 +26,15 @@ function setup() {
 
   // အကွက်များ နေရာချခြင်း
   for (var i = 0; i < cols; i++) {
-     if(i%2 === 1){
-       grid[i + 0 * cols].value = 0;
-       grid[i + 2 * cols].value = 0;
-       grid[i + 8 * cols].value = 1;
-     }else{
-       grid[i + 1 * cols].value = 0;
-       grid[i + 7 * cols].value = 1;
-       grid[i + 9 * cols].value = 1;
-     }
+      if(i%2 === 1){
+        grid[i + 0 * cols].value = 0;
+        grid[i + 2 * cols].value = 0;
+        grid[i + 8 * cols].value = 1;
+      }else{
+        grid[i + 1 * cols].value = 0;
+        grid[i + 7 * cols].value = 1;
+        grid[i + 9 * cols].value = 1;
+      }
   }
 
   // PeerJS Multiplayer Setup
@@ -47,20 +47,20 @@ function setup() {
     if(myIdDisplay) myIdDisplay.innerText = id;
   });
 
-  // တစ်ဖက်လူက လှမ်းချိတ်တာကို လက်ခံခြင်း (သူက Player 2 ဖြစ်သွားမယ်)
+  // တစ်ဖက်လူက လှမ်းချိတ်တာကို လက်ခံခြင်း (Host)
   peer.on('connection', function(c) {
     conn = c;
-    turnValue = 0; // Host က အဖြူ (Player 1)
+    turnValue = 0; // Host က အဖြူ
     setupConn();
   });
 
-  // Connect ခလုတ်နှိပ်ပြီး လှမ်းချိတ်ခြင်း (ကိုယ်က Player 2 ဖြစ်သွားမယ်)
+  // Join တဲ့သူ (Client)
   var connectBtn = document.getElementById('connect-btn');
   if(connectBtn) {
     connectBtn.onclick = function() {
       var remoteId = document.getElementById('peer-id-input').value;
       conn = peer.connect(remoteId);
-      turnValue = 1; // Join တဲ့သူက အမည်း (Player 2)
+      turnValue = 1; // Join တဲ့သူက အမည်း
       setupConn();
     };
   }
@@ -70,17 +70,22 @@ function setupConn() {
   if(statusDisplay) statusDisplay.innerText = "အခြေအနေ: ချိတ်ဆက်မိပါပြီ! သင်က Player " + (turnValue + 1) + " ပါ။";
   
   conn.on('data', function(data) {
-    // တစ်ဖက်လူဆီက Grid Data ရရင် ကိုယ့်ဆီမှာ Update လုပ်မယ်
-    for(var i = 0; i < grid.length; i++){
-      grid[i].value = data[i].value;
-      grid[i].highlight = false;
-      grid[i].kill = false;
+    // တစ်ဖက်လူဆီက ရလာတဲ့ values တွေကိုပဲ ကိုယ့် grid ထဲ ပြန်ထည့်မယ်
+    if (data.gridValues) {
+      for(var i = 0; i < grid.length; i++){
+        grid[i].value = data.gridValues[i];
+        grid[i].highlight = false;
+        grid[i].kill = false;
+        grid[i].color = grid[i].nativeColor;
+      }
+      turn = data.nextTurn; // တစ်ဖက်လူ ပို့လိုက်တဲ့ အလှည့်အတိုင်း ပြောင်းမယ်
+      checkWin();
     }
-    turn = (turn === 0) ? 1 : 0; // အလှည့်ပြောင်းမယ်
   });
 }
 
 function mouseClicked() {
+  // ကိုယ့်အလှည့်ဖြစ်မှ နှိပ်လို့ရမယ်
   if(turnValue === turn){
     if(!gameover && conn && conn.open){
       for (var i = 0; i < grid.length; i++) {
@@ -102,7 +107,6 @@ function mouseClicked() {
   }
 }
 
-// ရိုးရိုးရွှေ့သည့် Logic
 function executeNormalMove(i) {
   selected.value = -1;
   selected.color = selected.nativeColor;
@@ -111,43 +115,41 @@ function executeNormalMove(i) {
   completeTurn();
 }
 
-// စားသည့် Logic
 function executeKillMove(i) {
-  if(grid[i].value === 0) whites--;
-  else if(grid[i].value === 1) blacks--;
-  
-  selected.value = -1;
-  selected.color = selected.nativeColor;
-  
   var s = selected;
   var t = grid[i];
-  var targetIndex;
+  var targetIndex = -1;
 
+  // စားမယ့်အကွက်ကို တွက်ချက်ခြင်း
   if(s.i - t.i === 1 && s.j - t.j === 1) targetIndex = index(t.i - 1, t.j - 1);
   else if(s.i - t.i === -1 && s.j - t.j === 1) targetIndex = index(t.i + 1, t.j - 1);
   else if(s.i - t.i === 1 && s.j - t.j === -1) targetIndex = index(t.i - 1, t.j + 1);
   else if(s.i - t.i === -1 && s.j - t.j === -1) targetIndex = index(t.i + 1, t.j + 1);
 
-  resetHighlights();
-  t.value = -1;
-  if(targetIndex !== -1) grid[targetIndex].value = selectedValue;
-  completeTurn();
+  if(targetIndex !== -1 && grid[targetIndex].value === -1) {
+    if(grid[i].value === 0) whites--;
+    else if(grid[i].value === 1) blacks--;
+    
+    selected.value = -1;
+    selected.color = selected.nativeColor;
+    t.value = -1;
+    grid[targetIndex].value = selectedValue;
+    resetHighlights();
+    completeTurn();
+  }
 }
 
 function selectPiece(i) {
+  resetHighlights();
   grid[i].color = "gray";
   selected = grid[i];
   selectedValue = selected.value;
-  for (var j = 0; j < grid.length; j++) {
-    if (j !== i) grid[j].color = grid[j].nativeColor;
-    grid[j].highlight = false;
-    grid[j].kill = false;
-  }
   grid[i].checkMoves();
 }
 
 function resetHighlights() {
   for (var j = 0; j < grid.length; j++) {
+    grid[j].color = grid[j].nativeColor;
     grid[j].highlight = false;
     grid[j].kill = false;
   }
@@ -155,12 +157,29 @@ function resetHighlights() {
 
 function completeTurn() {
   turn = (turn === 0) ? 1 : 0;
-  // တစ်ဖက်လူဆီသို့ Grid data ပို့ခြင်း
-  conn.send(grid); 
+  
+  // Grid တစ်ခုလုံး မပို့ဘဲ value တွေကိုပဲ array တစ်ခုထဲ ထုတ်ယူမယ်
+  var valuesOnly = grid.map(cell => cell.value);
+  
+  // လိုအပ်တဲ့ data တွေကိုပဲ ပို့မယ်
+  conn.send({
+    gridValues: valuesOnly,
+    nextTurn: turn
+  });
+  
   checkWin();
 }
 
 function checkWin() {
+  // ကျားကောင်ရေကို grid ထဲကနေ တိုက်ရိုက်ပြန်စစ်တာ ပိုတိကျပါတယ်
+  let wCount = 0, bCount = 0;
+  for(let cell of grid) {
+    if(cell.value === 0) wCount++;
+    if(cell.value === 1) bCount++;
+  }
+  whites = wCount;
+  blacks = bCount;
+
   if(blacks === 0) { gameover = true; winner = 0; }
   else if(whites === 0) { gameover = true; winner = 1; }
 }
